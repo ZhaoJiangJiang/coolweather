5139306ae5d12eeaf1ed577e345055fc2b1b4c04
Tony <tony@gmail.com>
2018-08-03 14:55:48 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/android/coolweather/com/coolweather/WeatherActivity.java
+++ app/src/main/java/android/coolweather/com/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package android.coolweather.com.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.coolweather.com.coolweather.gson.Forecast;
 import android.coolweather.com.coolweather.gson.Weather;
@@ -90,7 +91,7 @@ public class WeatherActivity extends AppCompatActivity {
         String weatherString = prefs.getString("weather", null);
         final String weatherId;
         if (weatherString != null){//有缓存时直接解析天气数据
-            Weather weather = Utility.handleWeacherResponse(weatherString);
+            Weather weather = Utility.handleWeatherResponse(weatherString);
             weatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         }else {//无缓存时去服务器查询天气
@@ -150,7 +151,7 @@ public class WeatherActivity extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
                 //调用Utility.handleWeatherResponse()方法将返回的JSON数据转换成Weather对象
-                final Weather weather = Utility.handleWeacherResponse(responseText);
+                final Weather weather = Utility.handleWeatherResponse(responseText);
                 //将当前线程切换到主线程
                 runOnUiThread(new Runnable() {
                     @Override
@@ -206,6 +207,8 @@ public class WeatherActivity extends AppCompatActivity {
     处理并展示Weather实体类中的数据
      */
     private void showWeatherInfo(Weather weather){
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature+ "°C";
